f7d990bb65a69ad26cf326db94edba4ff58c75f9
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-10 18:58:45 +0100

    Setup

---
+++ src/main/java/dev/Innocent/EventManagementSystem/EventManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent.EventManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementSystemApplication.class, args);
+	}
+
+}
